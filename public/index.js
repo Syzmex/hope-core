db11191bf5fea57a86828cd50a495caac0fb06f5
mArker <252133226@qq.com>
2017-07-14 10:37:02 +0800

    test json parse error

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ console.log( hope );
 
 const { Ajax } = hope;
 
-const ajax = Ajax( '/index.html' ).then(( a ) => {console.log(a)
+const ajax = Ajax( '/test.json' ).then(( a ) => {console.log(a)
   console.log(typeof a)
   // alert( 1 );
   // alert( typeof a );
